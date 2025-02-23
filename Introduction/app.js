355fbdb57f63ee84baf90de2be698416614dbaf3
Vanshaj P Mohan <VanshajKumar145@gmail.com>
2025-02-23 18:51:51 +0530

    Files

--- Introduction/app.js
+++ Introduction/app.js
@@ -1,5 +1,5 @@
-let btn = document.querySelector("button");
-console.dir(btn);
+// let btn = document.querySelector("button");
+// console.dir(btn);
 
 // So we can assign our event property to such functions and then execute them.
 // btn.onclick = function () {
@@ -7,20 +7,31 @@ console.dir(btn);
 //     console.log("Button was clicked");
 // }
 
+
+// we can select multiple buttons too
+let btns = document.querySelectorAll("button");
+
 // Another way of doing it
 function sayHello(){
     alert("Alert says Hello");
 }
 
-btn.onclick = sayHello;
+function sayName(){
+    alert("Alert asks for Name");
+}
 
-// we can select multiple buttons too
-let btns = document.querySelectorAll("button");
+// btn.onclick = sayHello;
+
 
 for(btn of btns){
-    btn.onclick = sayHello;
-    btn.onmouseenter = function (){ //just move the cursor on the button and it will show us this in the console.
-        console.log("You enter a number");
-    }
-    console.dir(btn);
+    // btn.onclick = sayHello;
+    // btn.onmouseenter = function (){ //just move the cursor on the button and it will show us this in the console.
+    //     console.log("You enter a number");
+    // }
+    // btn.addEventListener("click", sayHello);
+    // btn.addEventListener("click", sayName);
+    btn.addEventListener("dblclick", function (){ //we can also add double click event too in this.
+        console.log("You double clicked the button");
+    });
+    // console.dir(btn);
 }

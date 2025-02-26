d70fccc11c6190a2ddd0966b6aa1cf846debb35d
Vanshaj P Mohan <VanshajKumar145@gmail.com>
2025-02-26 23:56:33 +0530

    files

--- Activity/Sub file/app.js
+++ Activity/Sub file/app.js
@@ -1,7 +1,26 @@
+p = document.querySelector("p");
+h1 = document.querySelector("h1");
+h3 = document.querySelector("h3");
+
 btn = document.querySelector("button");
 
 btn.addEventListener("click", function(){
     console.log(this);
     console.dir(this);
     console.dir(this.innerText);
+});
+
+p.addEventListener("click", function(){
+    console.dir(this.innerText);
+    this.style.backgroundColor = "blue";
+});
+
+h1.addEventListener("click", function(){
+    console.dir(this.innerText);
+    this.style.backgroundColor = "blue";
+});
+
+h3.addEventListener("click", function(){
+    console.dir(this.innerText);
+    this.style.backgroundColor = "blue";
 });

b9f2158b23ae95188ef5bbf36a6e89dbaac932a2
Vanshaj P Mohan <VanshajKumar145@gmail.com>
2025-03-07 22:45:54 +0530

    files

--- Activity/Events/Form Events/app.js
+++ Activity/Events/Form Events/app.js
@@ -1,6 +1,19 @@
 let form = document.querySelector("form")
 
+// form.addEventListener("submit", function(event){ //sumbmit here is an event
+//     event.preventDefault(); //to prevent default action from event
+//     alert("Form Submitted");
+// });
+
+//And we will learn how to access data from a form once we submit it.
 form.addEventListener("submit", function(event){ //sumbmit here is an event
     event.preventDefault(); //to prevent default action from event
-    alert("Form Submitted");
+    let user = document.querySelector("#user");
+    let pass = document.querySelector("#pass");
+    console.log("Username is", user.value);
+    console.log("Password is", pass.value);
+    alert(`Hi ${user.value}, your password is set to ${pass.value}`); //creating an alert
+    // console.dir(inp);
+    // console.log(inp);
+    // console.log(inp.value);
 });

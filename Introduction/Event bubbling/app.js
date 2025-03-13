5f7c7d31e240c914605ce36aa9d7bb132efaf908
Vanshaj P Mohan <VanshajKumar145@gmail.com>
2025-03-13 22:10:32 +0530

    files

--- Introduction/Event bubbling/app.js
+++ Introduction/Event bubbling/app.js
@@ -6,14 +6,18 @@ div.addEventListener("click", function(){
     console.log("Div was clicked");
 });
 
-ul.addEventListener("click", function(){
+ul.addEventListener("click", function(event){
+    event.stopPropagation();
     console.log("Ul was clicked");
 });
 
 for(li of lis){ //so if we click on list it will trigger it's parent elements too.
-    li.addEventListener("click", function(){
+    li.addEventListener("click", function(event){
+        event.stopPropagation();
         console.log("Li was clicked");
     });
 }
 
+//So this is what we call Event Bubbling, where the event is triggered on the child element and then it's parent elements too.
+//And to stop this we use event.stopPropagation() method. It stops our Event Bubbling
 
